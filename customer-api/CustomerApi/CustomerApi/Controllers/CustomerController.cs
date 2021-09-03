5247fb38adf44f21059e6123a69e8bb66f6fc39d
sugathb <sugathb@gmail.com>
2021-09-04 00:14:18 +1000

    customer-api

---
+++ customer-api/CustomerApi/CustomerApi/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CustomerApi.Models;
+
+namespace CustomerApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var customer = new Customer
+            {
+                CustomerId = 1,
+                FirstName = "John",
+                LastName = "Pfizer",
+                Address = "Sample Address 1",
+                Mobile = "0444111111",
+                Email = "test@testmail.com"
+            };
+
+            return Ok(customer);
+        }
+    }
+}
