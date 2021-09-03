7d171c942942920a0298e7e9ecda308a072318db
sugathb <sugathb@gmail.com>
2021-09-04 00:19:33 +1000

    order-api

---
+++ order-api/OrderApi/OrderApi/Controllers/OrderController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderApi.Models;
+
+namespace OrderApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+       
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var order = new Order {OrderId = 1, CustomerId = 1, TotalPrice = 10};
+            return Ok(order);
+        }
+    }
+}
---
+++ order-api/OrderApi/OrderApi/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderApi.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
