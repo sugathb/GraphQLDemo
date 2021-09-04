629ee52a6f014ddd37a1514a4b4d63df1a2fe05f
sugathb <sugathb@gmail.com>
2021-09-05 01:06:08 +1000

    basic-query-added

---
+++ graphql-gateway/GraphQLGateway/GraphQLGateway/GraphQL/Query.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using GraphQLGateway.Models;
+
+namespace GraphQLGateway.GraphQL
+{
+    public class Query
+    {
+        public IEnumerable<Order> GetOrder()
+        {
+            return new List<Order>()
+            {
+                new Order
+                {
+                    Id = 1,
+                    CustomerId = 1,
+                    TotalPrice = 10
+                },
+                new Order
+                {
+                    Id = 2,
+                    CustomerId = 2,
+                    TotalPrice = 20
+                },
+                new Order
+                {
+                    Id = 3,
+                    CustomerId = 3,
+                    TotalPrice = 30
+                }
+            };
+        }
+        public IEnumerable<Customer> GetCustomer()
+        {
+            return new List<Customer>()
+            {
+                new Customer
+                {
+                    Id = 1,
+                    FirstName = "John",
+                    LastName = "Ford",
+                    Address = "Address 1",
+                    Email = "test1@testmail.com",
+                    Mobile = "0449111111"
+                },
+                new Customer
+                {
+                    Id = 1,
+                    FirstName = "Angus",
+                    LastName = "Bowmen",
+                    Address = "Address 2",
+                    Email = "test2@testmail.com",
+                    Mobile = "0449111112"
+                },
+                new Customer
+                {
+                    Id = 1,
+                    FirstName = "Mark",
+                    LastName = "Cater",
+                    Address = "Address 3",
+                    Email = "test3@testmail.com",
+                    Mobile = "0449111113"
+                }
+            };
+        }
+    }
+}
--- graphql-gateway/GraphQLGateway/GraphQLGateway/Startup.cs
+++ graphql-gateway/GraphQLGateway/GraphQLGateway/Startup.cs
@@ -1,6 +1,6 @@
+using GraphQLGateway.GraphQL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +12,8 @@ namespace GraphQLGateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddGraphQLServer()
+                .AddQueryType<Query>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,10 +28,7 @@ namespace GraphQLGateway
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapGraphQL();
             });
         }
     }
